b9fde70e34c675af3be5678ff4482df2eb5c0a01
ivarejao <imsvarejao@gmail.com>
2021-12-14 15:42:46 -0300

    vibdata added

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='vibenet',
+    name='vibdata',
     version='0.2',
     url='https://gitlab.com/ninfa-ufes/deep-rpdbcs/signal-datasets.git',
-    license='MIT',
+    license='private',
     author='Lucas Henrique Sousa Mello',
     author_email='lucashsmello@gmail.com',
-    description='A DeepLearn approuch to vibrational signals for bearing fault classification',
-    packages=['datahandler', 'datahandler.PU', 'datahandler.SEU', 'datahandler.CWRU', 'datahandler.MFPT'],
+    description='A DeepLearn approch to vibrational signals for bearing fault classification',
+    packages=['vibdata', 'vibdata.datahandler', 'vibdata.datahandler.PU', 'vibdata.datahandler.SEU', 'vibdata.datahandler.CWRU', 'vibdata.datahandler.MFPT'],
     install_requires=[
         'pandas',
         'numpy',
