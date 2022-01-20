651ffa99b6d867eacd8f5c3b95af328bbd0a3091
joluan <joluan.zuca@gmail.com>
2022-01-20 16:46:07 -0300

    UOC Gear Fault Data added.

--- setup.py
+++ setup.py
@@ -2,16 +2,17 @@ from setuptools import setup
 
 setup(
     name='vibdata',
-    version='0.4.4',
+    version='0.4.5',
     url='https://gitlab.com/ninfa-ufes/deep-rpdbcs/signal-datasets.git',
     license='private',
     author='Lucas Henrique Sousa Mello',
     author_email='lucashsmello@gmail.com',
     description='A library for loading vibration signals datasets',
     packages=['vibdata', 'vibdata.datahandler', 'vibdata.datahandler.PU', 'vibdata.datahandler.SEU', 'vibdata.datahandler.CWRU',
-     'vibdata.datahandler.MFPT', 'vibdata.datahandler.RPDBCS', 'vibdata.datahandler.transforms'],
+     'vibdata.datahandler.MFPT', 'vibdata.datahandler.RPDBCS','vibdata.datahandler.UOC', 'vibdata.datahandler.transforms'],
     package_data={'vibdata.datahandler.PU': ['PU.csv'], 'vibdata.datahandler.SEU': ['SEU.csv'],
-                  'vibdata.datahandler.CWRU': ['CWRU.csv'], 'vibdata.datahandler.MFPT': ['MFPT.csv']},
+                  'vibdata.datahandler.CWRU': ['CWRU.csv'], 'vibdata.datahandler.UOC': ['UOC.csv'],
+                  'vibdata.datahandler.MFPT': ['MFPT.csv']},
     install_requires=[
         'pandas',
         'numpy',
