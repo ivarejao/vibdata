c2468b7da77edd11aa11db67b145fd452058c96a
Lucas Henrique Sousa Mello <lucashsmello@gmail.com>
2022-01-21 14:00:13 -0300

    Documentation of TransformDataset

--- vibdata/datahandler/transforms/TransformDataset.py
+++ vibdata/datahandler/transforms/TransformDataset.py
@@ -1,5 +1,4 @@
 
-from pandas.core.frame import DataFrame
 from torch.utils.data import BatchSampler, SequentialSampler, DataLoader, Dataset
 import pandas as pd
 from vibdata.datahandler.base import RawVibrationDataset
@@ -8,10 +7,54 @@ import os
 import pickle
 from tqdm import tqdm
 import numpy as np
-import torch
+from vibdata.datahandler.transforms.signal import Sequential
 
 
-def transform_and_saveDataset(dataset: RawVibrationDataset, transforms, dir_path: str, batch_size=1024):
+class PickledDataset(Dataset):
+    def __init__(self, root_dir, transforms=None) -> None:
+        super().__init__()
+        self.root_dir = root_dir
+        self.file_names = [f for f in os.listdir(self.root_dir) if f[-4:] == '.pkl' and f != 'metainfo.pkl']
+        self.file_names = sorted(self.file_names, key=lambda k: int(k[:-4]))
+        with open(os.path.join(root_dir, 'metainfo.pkl'), 'rb') as f:
+            self.metainfo = pickle.load(f)
+        self.transforms = transforms
+        assert(len(self.file_names) == len(self.metainfo['label'])), "%d %d" % (
+            len(self.file_names), len(self.metainfo['label']))
+
+    def __getitem__(self, i):
+        ret = {k: v.iloc[i] if isinstance(v, pd.DataFrame) else v[i]
+               for k, v in self.metainfo.items()}
+        fpath = os.path.join(self.root_dir, self.file_names[i])
+        with open(fpath, 'rb') as f:
+            ret['signal'] = pickle.load(f)
+
+        if(self.transforms is not None):
+            if(hasattr(self.transforms, 'transform')):
+                return self.transforms.transform(ret)
+            return self.transforms(ret)
+        return ret
+
+    def __len__(self):
+        return len(self.file_names)
+
+
+def transform_and_saveDataset(dataset: RawVibrationDataset, transforms, dir_path: str, batch_size=1024) -> PickledDataset:
+    """
+    This function applies `transform` to `dataset` and caches each transformed sample in a separated file in `dir_path`, 
+    and finally returns a Dataset object implementing `__getitem__`.
+    If this function is called with the same arguments a second time, it returns the cached dataset. 
+
+    Args:
+        dataset: Should be `RawVibrationDataset` object implementing `__getitem__` that accepts lists of integers as parameters.
+        transforms: A object (or a list of objects) implementing `__call__` or `transform`
+        dir_path: path to the cache directory (Suggestion: use "/tmp" or another temporary directory)
+        batch_size: 
+    """
+    if(not hasattr(transforms, 'transform') and not callable(transforms)):
+        if(hasattr(transforms, '__iter__')):
+            transforms = Sequential(*transforms)
+
     m = hashlib.md5()
     to_encode = [transforms, dataset.__class__.__name__, len(dataset), dataset.getMetaInfo()]
     for e in to_encode:
@@ -29,7 +72,7 @@ def transform_and_saveDataset(dataset: RawVibrationDataset, transforms, dir_path
     else:
         os.makedirs(dir_path)
 
-    dataloader = DataLoader(dataset, batch_size=None, collate_fn=lambda x:x, # do not convert to Tensor
+    dataloader = DataLoader(dataset, batch_size=None, collate_fn=lambda x: x,  # do not convert to Tensor
                             sampler=BatchSampler(SequentialSampler(dataset), batch_size, False))
     metainfo_list = []
     fid = 0
@@ -63,33 +106,3 @@ def transform_and_saveDataset(dataset: RawVibrationDataset, transforms, dir_path
         f.write(hash_code)
 
     return PickledDataset(dir_path)
-
-
-class PickledDataset(Dataset):
-    def __init__(self, root_dir, transforms=None) -> None:
-        super().__init__()
-        self.root_dir = root_dir
-        self.file_names = [f for f in os.listdir(self.root_dir) if f[-4:] == '.pkl' and f != 'metainfo.pkl']
-        self.file_names = sorted(self.file_names, key=lambda k: int(k[:-4]))
-        with open(os.path.join(root_dir, 'metainfo.pkl'), 'rb') as f:
-            self.metainfo = pickle.load(f)
-        self.transforms = transforms
-        assert(len(self.file_names) == len(self.metainfo['label'])), "%d %d" % (
-            len(self.file_names), len(self.metainfo['label']))
-
-    def __getitem__(self, i):
-        ret = {k: v.iloc[i] if isinstance(v, pd.DataFrame) else v[i]
-               for k, v in self.metainfo.items()}
-        fpath = os.path.join(self.root_dir, self.file_names[i])
-        with open(fpath, 'rb') as f:
-            ret['signal'] = pickle.load(f)
-
-        if(self.transforms is not None):
-            if(hasattr(self.transforms, 'transform')):
-                return self.transforms.transform(ret)
-            return self.transforms(ret)
-        return ret
-
-    def __len__(self):
-        return len(self.file_names)
-
