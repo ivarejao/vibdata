707074192554a9851598f1abd0cca1fb8e47b5b7
Igor Varejão <imsvarejao@gmail.com>
2023-02-08 17:32:55 -0300

    Create `labels.csv` with all labels centralized, except SEU and FEMFTO

---
+++ vibdata/definitions.py
@@ -0,0 +1,6 @@
+# Code made in Pycharm by Igor Varejao
+import os.path
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+RESOURCES_DIR = os.path.join(ROOT_DIR, 'resources')
+LABELS_PATH = os.path.join(RESOURCES_DIR, 'labels.csv')
