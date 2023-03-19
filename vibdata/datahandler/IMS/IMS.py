c03ecc2c19bbdb94e516bd72e67457b12fcc4fad
Igor Varejão <imsvarejao@gmail.com>
2023-03-19 17:49:04 -0300

    IMS dataset improvements: - Refactor 'bearing.position' column into two columns 'bearing' and 'axis' - Add frequency rotation to meta-info file - Optimize code

--- vibdata/datahandler/IMS/IMS.py
+++ vibdata/datahandler/IMS/IMS.py
@@ -1,27 +1,69 @@
 # Code made in Pycharm by Igor Varejao
 import os
-
 from vibdata.definitions import LABELS_PATH
-
 from vibdata.datahandler.utils import _get_package_resource_dataframe
-
 import numpy as np
 import pandas as pd
-
 from vibdata.datahandler.base import RawVibrationDataset, DownloadableDataset
-from .Tests.Test1 import FirstTest
-from .Tests.Test2 import SecondTest
-from .Tests.Test3 import ThirdTest
+
+# This dataset are composed by three tests each one describing a test-to-failure experiment.
+# Each test is made by several files, representing a ~1 second record with 20Hz sample rate.
+# Classification is based on the paper that can be found in https://www.sciencedirect.com/science/article/abs/pii/S0957417418303324
+# ---------------------------------------------------------
+#                           Test 1
+# ---------------------------------------------------------
+# In this test, 8 accelerometers were placed in 4 bearings in vertical and horizontal axis.
+# Each file consists of 20480 lines, with 8 channels where every pair
+# indicating the record of x-axis and y-axis of the bearing
+# The relation between column and bearing is
+# | Bearing | Colunms |
+# |---------|---------|
+# | 1       | 0 & 1   |
+# | 2       | 2 & 3   |
+# | 3       | 4 & 5   |
+# | 4       | 6 & 7   |
+#
+# Only Bearing 3 and Bearing 4 got Faults States, therefore, are recommended to use them.
+# In this test it got recorded 2156 times / files.
+# | 			Bearing 3       	|
+# |---------------------------------|
+# | Healthy State       |   Range   |
+# |---------------------|-----------|
+# | Normal              | 0:1799    |
+# | Degraded Inner Race | 1800:2099 |
+# | Inner Race Fault    | 2100:2155 |
+#
+# | 			Bearing 4       	 |
+# |----------------------------------|
+# | Healthy State        |   Range   |
+# |----------------------|-----------|
+# | Normal               | 0:1399    |
+# | Degraded Roller Race | 1400:1849 |
+# | Roller race Fault    | 1850:2155 |
+#
+# ---------------------------------------------------------
+#                           Test 2
+# ---------------------------------------------------------
+# Only Bearing 1 got Faults States, therefore, are recommended to use only it.
+# In this test it got recorded 984 times / files. It got 4 channels each one for each
+# bearing.
+# | 			Bearing 1       	|
+# |---------------------------------|
+# | Healthy State       |   Range   |
+# |---------------------|-----------|
+# | Normal              | 0:699     |
+# | Degraded Outer Race | 700:949   |
+# | Outer Race Fault    | 950:983   |
+#
+# ---------------------------------------------------------
+#                           Test 3
+# ---------------------------------------------------------
+# In this test no bearing got a Fault, and it got recorded 6.324 times / files
 
 
 class IMS_raw(RawVibrationDataset, DownloadableDataset):
 
     source = "https://drive.google.com/file/d/1r9SadjRcUkvI1wJZvi-nu9VzPyQ8oOvE/view?usp=sharing"
-    DATAFILE_NAMES = {
-        '1stTest': FirstTest.file_names,
-        '2ndTest': SecondTest.file_names,
-        '3rdTest': ThirdTest.file_names
-    }
 
     urls = mirrors = ["1r9SadjRcUkvI1wJZvi-nu9VzPyQ8oOvE"]  # Google drive id
     resources = [('IMS.zip', '4d24ffef04f5869d68c0bc7cf65ebf77')]
@@ -44,9 +86,6 @@ class IMS_raw(RawVibrationDataset, DownloadableDataset):
     #                        '2nd_test.rar': '32893c492d76c9d3efe9130227f36af5',
     #                        '3rd_test.rar': '11147ea5a16ceaeb5702f3340a72811a'},
     #
-    #              'Files': {'1st_test.rar': FirstTest.md5sums_files,
-    #                        '2nd_test.rar': SecondTest.md5sums_files,
-    #                        '3rd_test.rar': ThirdTest.md5sums_files}
     #              }
 
     def __init__(self, root_dir: str, download=False, with_thirdtest=False):
@@ -57,7 +96,15 @@ class IMS_raw(RawVibrationDataset, DownloadableDataset):
             super().__init__(root_dir=root_dir, download_resources=IMS_raw.resources, download_mirrors=None)
         self.third_test = with_thirdtest
 
-    def __getTest(self, ntest):
+    def _get_test_folder(self, ntest : int) -> str:
+        """
+        Get the name of the folder where the files of the test will be stored
+        Args:
+            ntest (int): The number of the test occurred
+
+        Returns:
+            The name of the folder containing the files of test in matter
+        """
         if ntest == 1:
             return '1st_test'
         elif ntest == 2:
@@ -83,16 +130,15 @@ class IMS_raw(RawVibrationDataset, DownloadableDataset):
             print(f"{error} with idx={idx}")
             exit(1)
 
-        file_name = rows['file_name']
-        tests = rows['test']
-        bear_name = rows['bearing.position']
-        signal_datas = np.empty(len(bear_name), dtype=object)
+        signal_datas = np.empty(rows.shape[0], dtype=object)
 
-        for i, (f, b, t) in enumerate(zip(file_name, bear_name, tests)):
-            path_file = f"{self.__getTest(t)}/{f}"
-            file_data = np.loadtxt(os.path.join(self.raw_folder, path_file), delimiter='\t', unpack=True)
-            signal_datas[i] = file_data[FirstTest.back_bearing(b), :]
-        signal_datas = signal_datas
+        for i, row in enumerate(rows.itertuples()):
+            path_file = os.path.join(self.raw_folder,
+                                     self._get_test_folder(row.test),
+                                     row.file_name)
+            file_data = np.loadtxt(path_file, delimiter='\t', unpack=True)
+            column = (row.bearing - 1) * 2 + (1 if row.axis == "vertical" else 0)
+            signal_datas[i] = file_data[column, :]
 
         return {'signal': signal_datas, 'metainfo': rows}
 
